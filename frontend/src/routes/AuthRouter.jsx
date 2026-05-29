72a463b2a629f69e05072df85156274a6e57fc4b
Rohit Gupta <rohitgupta122343@gmail.com>
2026-05-29 19:14:33 +0530

    update

--- frontend/src/routes/AuthRouter.jsx
+++ frontend/src/routes/AuthRouter.jsx
@@ -1,11 +1,11 @@
 
 import React from 'react'
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
-import Login from '../features/auth/pages/login'
-import Register from '../features/auth/pages/register'
+import Login from '../features/auth/pages/Login'
+import Register from '../features/auth/pages/Register'
 import Feed from '../features/post/pages/Feed'
 import CreatePost from '../features/post/pages/CreatePost'
-import Profile from '../features/users/pages/profile'
+import Profile from '../features/users/pages/Profile'
 import ProtectedRoute from '../features/auth/components/ProtectedRoute'
 
 const AuthRouter = () => {
