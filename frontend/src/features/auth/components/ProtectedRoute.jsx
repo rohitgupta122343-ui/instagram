874d6d4aede112af44470cf3c7cac302410a22ad
Rohit Gupta <rohitgupta122343@gmail.com>
2026-05-29 19:41:51 +0530

    added

--- frontend/src/features/auth/components/ProtectedRoute.jsx
+++ frontend/src/features/auth/components/ProtectedRoute.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Navigate } from 'react-router-dom'
-import Cookies from 'js-cookie'
+import axios from 'axios'
+
 const ProtectedRoute = ({ children }) => {
 
-  const token = Cookies.get("token")
+   const [loading, setLoading] = useState(true)
+   const [isAuth, setIsAuth] = useState(false)
 
-  if (!token) {
-    return <Navigate to="/login" />
-  }
+   useEffect(() => {
 
-  return children
+      axios.get('https://instagram-y4ln.onrender.com/api/auth/get-me', {
+         withCredentials: true
+      })
+      .then(() => {
+         setIsAuth(true)
+      })
+      .catch(() => {
+         setIsAuth(false)
+      })
+      .finally(() => {
+         setLoading(false)
+      })
+
+   }, [])
+
+   if (loading) {
+      return <h1>Loading...</h1>
+   }
+
+   if (!isAuth) {
+      return <Navigate to="/login" />
+   }
+
+   return children
 }
 
 export default ProtectedRoute
